5b839be7c0f23f17413b0c1cbd28642a5bb22db1
Marko Jalonen <marko.jalonen@tuni.fi>
2023-11-30 14:40:52 +0200

    added an example of simulation stages

--- exercises/simstages/simstages.cpp
+++ exercises/simstages/simstages.cpp
@@ -11,3 +11,56 @@
 
 // there are different callback functions called during these stages (explained below)
 
+SC_MODULE(Stages)
+{
+    SC_CTOR(Stages)
+    {
+        std::cout << sc_time_stamp() << " : elaboration : constructor" << std::endl;
+        SC_THREAD(thread);
+    }
+
+    ~Stages()
+    {
+        std::cout << sc_time_stamp() << " : cleanup : destructor" << std::endl;
+    }
+
+    void thread()
+    {
+        std::cout << sc_time_stamp() << " : execution : init" << std::endl;
+        int i = 0;
+        while(true)
+        {
+            wait(1, SC_SEC);
+            std::cout << sc_time_stamp() << " : execution : simulation" << std::endl;
+            if(++i >= 3)
+            {
+                sc_stop();
+            }
+        }
+    }
+
+    // the callback funtions
+    void before_end_of_elaboration() 
+    {
+        std::cout << sc_time_stamp() << " : before end of elaboration" << std::endl;
+    }
+    void end_of_elaboration() 
+    {
+        std::cout << sc_time_stamp() << " : end of elaboration" << std::endl;
+    }
+    void start_of_simulation() 
+    {
+        std::cout << sc_time_stamp() << " : start of simulation" << std::endl;
+    }
+    void end_of_simulation() 
+    {
+        std::cout << sc_time_stamp() << " : end of simulation" << std::endl;
+    }
+};
+
+int sc_main(int argc, char** argv)
+{
+    Stages stages("stages");
+    sc_start();
+    return 0;
+}
